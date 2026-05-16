a5be1066663ec57c4e8019c7b40db47f10fbd1ff
Ashish jha <ajha5645@gmail.com>
2026-05-16 14:59:53 +0530

    feat: implement tenant management service and sidebar navigation component

--- src/app/core/services/tenant.service.ts
+++ src/app/core/services/tenant.service.ts
@@ -9,6 +9,22 @@ export class TenantService {
   constructor(private readonly http: HttpClient) {}
 
   getTenants(page = 0, size = 10) {
-    return this.http.get(`${this.baseUrl}?page=${page}&size=${size}`);
+    return this.http.get<any>(`${this.baseUrl}?page=${page}&size=${size}`);
+  }
+
+  approveTenant(id: string) {
+    return this.http.post<any>(`${this.baseUrl}/${id}/approve`, {});
+  }
+
+  activateTenant(id: string) {
+    return this.http.post<any>(`${this.baseUrl}/${id}/activate`, {});
+  }
+
+  deactivateTenant(id: string) {
+    return this.http.post<any>(`${this.baseUrl}/${id}/deactivate`, {});
+  }
+
+  suspendTenant(id: string) {
+    return this.http.post<any>(`${this.baseUrl}/${id}/suspend`, {});
   }
 }
